f559d287f67af76fde8ab40a1f42335fb73f053d
btevfik <bt@bu.edu>
2013-04-15 03:04:16 -0400

    added comment box to image page (db not implemented)

--- Image.aspx
+++ Image.aspx
@@ -7,6 +7,8 @@
     <script type="text/javascript" src="/scripts/jquery.autosize.js"></script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+
+    <!-- FACEBOOK JS SDK -->
     <div id="fb-root"></div>
     <script>(function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -16,6 +18,7 @@
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
     </script>
+
     <div class="content">
         <div class="outer">
             <div class="inner">
@@ -27,49 +30,88 @@
                 <asp:Label ID="imageDescription" runat="server" Text="Description of the image goes here"></asp:Label>
                 <br />
                 <br />
-                <div class="buttons">
-                    <asp:Button runat="server" ID="upButton" Text="Up &uarr;" CssClass="uploadButton" />
-                    <asp:TextBox runat="server" ID="upCount" Width="10" Text="0" Enabled="false" />
-                    &nbsp;&nbsp;
-                    <asp:Button runat="server" ID="downButton" Text="Down &darr;" CssClass="uploadButton" />
-                    <asp:TextBox runat="server" ID="downCount" Width="10" Text="0" Enabled="false" />
-                    &nbsp;&nbsp;
-                    <asp:Button runat="server" ID="cuddleBUtotn" Text="Cuddle &hearts;" CssClass="uploadButton" />
-                    <asp:TextBox runat="server" ID="cuddleCount" Width="10" Text="0" Enabled="false" />
-                     &nbsp;&nbsp;
-                    <fb:like send="true" layout="button_count" width="450" show_faces="true"></fb:like>
-                </div>
-                <div class="comments">
-                    <asp:LoginView ID="LoginView1" runat="server">
-                        <AnonymousTemplate>
-                            <asp:LoginStatus ID="LoginStatus1" runat="server" />
-                            to leave a comment.
-                        </AnonymousTemplate>
-                        <LoggedInTemplate>
-                            <asp:TextBox ID="AddCommentBox" Rows="1" Columns="60" placeholder="Add a comment..." TextMode="MultiLine" ClientIDMode="Static" runat="server"></asp:TextBox>
-                        </LoggedInTemplate>
-                    </asp:LoginView>
-                    <br />
-                    <br />
-                    <b>Comments</b>
-                    <br />
-                    <br />
-                </div>
+                <!--SCRIPT MANAGER FOR UPDATE PANEL -->
+                <asp:ScriptManager ID="ScriptManager1" runat="server">
+                </asp:ScriptManager>
+                <asp:UpdatePanel ID="updatepanel1" runat="server">
+                    <ContentTemplate>
+                        <div class="buttons">
+                            <asp:Button runat="server" ID="upButton" OnClick="upButton_Click" Text="Up &uarr;" CssClass="uploadButton" />
+                            <asp:TextBox runat="server" ID="upCount" Width="10" Text="0" Enabled="false" />
+                            &nbsp;&nbsp;
+                    <asp:Button runat="server" ID="downButton" OnClick="downButton_Click" Text="Down &darr;" CssClass="uploadButton" />
+                            <asp:TextBox runat="server" ID="downCount" Width="10" Text="0" Enabled="false" />
+                            &nbsp;&nbsp;
+                    <asp:Button runat="server" ID="cuddleButton" OnClick="cuddleButton_Click" Text="Cuddle &hearts;" CssClass="uploadButton" />
+                            <asp:TextBox runat="server" ID="cuddleCount" Width="10" Text="0" Enabled="false" />
+                            &nbsp;&nbsp;
+                    <fb:like send="true" layout="button_count" width="0" show_faces="true"></fb:like>
+                            <br />
+                            <asp:Label ID="error" runat="server" Style="color: red" Text=""></asp:Label>
+                        </div>
+                        <div class="comments">
+                            <asp:LoginView ID="LoginView1" runat="server">
+                                <AnonymousTemplate>
+                                    <asp:LoginStatus ID="LoginStatus1" runat="server" />
+                                    to leave a comment.
+                                </AnonymousTemplate>
+                                <LoggedInTemplate>
+                                    <asp:TextBox ID="AddCommentBox" Rows="1" Columns="60" placeholder="Add a comment..." TextMode="MultiLine" ClientIDMode="Static" runat="server"></asp:TextBox>
+                                </LoggedInTemplate>
+                            </asp:LoginView>
+                            <asp:Button ID="commentButton" CssClass="uploadButton" runat="server" OnClick="commentButton_Click" Style="display: none;" Text="Comment"></asp:Button>
+                            <br />
+                            <br />
+                            <b>Comments</b>
+                            <asp:Panel ID="commentPanel" runat="server"></asp:Panel>
+                            <br />
+                        </div>
+                    </ContentTemplate>
+                </asp:UpdatePanel>
             </div>
         </div>
         <div class="clear"></div>
     </div>
+
+    <!-- COMMENT ON ENTER KEY PRESSED-->
     <script>
-        $(document).ready(function () {
+        function pageLoad() {
             $("#AddCommentBox").keypress(function (e) {
                 var code = (e.keyCode ? e.keyCode : e.which);
                 if (code == 13) { //Enter keycode
-                    $('#AddCommentBox').val("");
-                    __doPostBack('__Page', 'something');
+                    var comment = $('#AddCommentBox').val();
+                    //simulate the button click
+                    __doPostBack("<%=commentButton.UniqueID %>", "");
                 }
             });
             $('textarea').autosize();
-        });
+        }
+    </script>
+
+    <!-- PARSE FB AFTER UPDATE PANEL IS UPDATED -->
+    <script type="text/javascript">
+
+        var prm = Sys.WebForms.PageRequestManager.getInstance();
+        prm.add_pageLoaded(pageLoaded);
+
+        var _panels, _count;
+
+        function pageLoaded(sender, args) {
+            if (_panels != undefined && _panels.length > 0) {
+                for (i = 0; i < _panels.length; i++)
+                    _panels[i].dispose();
+            }
+
+            var panels = args.get_panelsUpdated();
+
+            if (panels.length > 0) {
+                updateFbLike();
+            }
+        }
+
+        function updateFbLike() {
+            FB.XFBML.parse();
+        }
     </script>
 </asp:Content>
 
--- Image.aspx.cs
+++ Image.aspx.cs
@@ -7,13 +7,22 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Web.Security;
+using System.Web.Services;
 
 public partial class Image : System.Web.UI.Page
 {
+    //if of image
+    string id;
+    //current user
+    MembershipUser u;
+    //comments in the session
+    List<Literal> persistControls = new List<Literal>();
+
     protected void Page_Load(object sender, EventArgs e)
     {
         //get the requested img
-        string id = Request.QueryString["id"];
+        id = Request.QueryString["id"];
         //set img
         string url = "/ShowImage.ashx?imgid=" + id;
         Image1.ImageUrl = url;
@@ -21,32 +30,76 @@ public partial class Image : System.Web.UI.Page
         if (Page.User.Identity.IsAuthenticated)
         {
             TextBox commentBox = LoginView1.FindControl("AddCommentBox") as TextBox;
-            commentBox.Width = getWidth(id);
+            commentBox.Width = getWidth(id); //later something like.. FuddleImage.getWidth(id);
         }
         //set widths of counts
         upCount.Width = upCount.Text.Length * 8;
         downCount.Width = downCount.Text.Length * 8;
         cuddleCount.Width = cuddleCount.Text.Length * 8;
+
+        //set the voting counts
+        /*
+         upCount.Text  = FuddleImage.getUpCount(id);
+         downCount.Text = FuddleImage.getDownCount(id);
+         cuddleCount.Text = FuddleImage.getCuddleCount(id);
+         */
+
+         //set the title of the page
+        /*
+            Page.Header.Title = "Fuddle | " + FuddleImage.getTitle(id);
+         */
+
+        //set title and description of image
+        /*
+        imageTitle.Text = FuddleImage.getTitle(id);
+        imageDescription.Text = FuddleImage.getDesc(id);
+         */
+
+        //load comments from database
+        loadComments();
+
+        //load session comments (we wont need when database is implemented)
+        // if you already have some literal populated
+        if (Session["persistControls"] != null)
+        {
+            // pull them out of the session
+            persistControls = (List<Literal>)Session["persistControls"];
+            foreach (Literal ltrls in persistControls)
+                commentPanel.Controls.AddAt(0, ltrls); // and push them back into the page
+        }
+
+        //for simulating button click from javascript when enter is pressed
+        ScriptManager.GetCurrent(this).RegisterAsyncPostBackControl(this.commentButton);
+
     }
 
 
     //can be moved to a separete image class for reuse
-     protected int getWidth(string id){
-     SqlDataReader rdr = null;
+    protected int getWidth(string id)
+    {
+        SqlDataReader rdr = null;
         SqlConnection conn = new SqlConnection();
         SqlCommand cmd = new SqlCommand();
-        int width=300;
+        int width = 300;
         try
         {
             string connStr = ConfigurationManager.ConnectionStrings["fuddleConnectionString"].ConnectionString;
             conn = new SqlConnection(connStr);
 
-            cmd = new SqlCommand("SELECT Image_width FROM [Image_table] WHERE Image_id = " + id, conn);
+            cmd = new SqlCommand("SELECT Image_width FROM [Image_table] WHERE Image_id = @id", conn);
+            if (id == null)
+            {
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = DBNull.Value;
+            }
+            else
+            {
+                cmd.Parameters.Add("@id", SqlDbType.Int, 16).Value = id;
+            }
             conn.Open();
             rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                width = (int )rdr["Image_width"];
+                width = (int)rdr["Image_width"];
                 if (width > 800)
                 {
                     width = 800;
@@ -61,6 +114,112 @@ public partial class Image : System.Web.UI.Page
             conn.Close();
             conn.Dispose();
         }
-         return width+10;
+        return width + 10;
+    }
+
+    //upvoting this image
+    protected void upButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            u = Membership.GetUser();
+            if (u == null) throw new Exception();
+            /*
+            bool result = FuddleImage.upVote(id,u.UserName);
+            if(result == false){
+                error.Text = "Error voting."
+            }
+            else{
+             */
+            upCount.Text = Int32.Parse(upCount.Text) + 1 + "";
+            // }
+        }
+        catch
+        {
+            error.Text = "You are not logged in.";
+        }
+    }
+
+    //down voting this image
+    protected void downButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            u = Membership.GetUser();
+            if (u == null) throw new Exception();
+            /*
+            bool result = FuddleImage.downVote(id,u.UserName);
+            if(result == false){
+                error.Text = "Error voting."
+            }
+            else{
+             */
+             downCount.Text = Int32.Parse(downCount.Text) - 1 + ""; 
+             // }
+        }
+        catch
+        {
+            error.Text = "You are not logged in.";
+        }
+    }
+
+    //cuddling this image
+    protected void cuddleButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            u = Membership.GetUser();
+            if (u == null) throw new Exception();
+            /*
+            bool result = FuddleUser.cuddleImage(id);
+            if(result == false){
+                error.Text = "Error cuddling."
+            }
+            else{
+             */
+            cuddleCount.Text = Int32.Parse(cuddleCount.Text) + 1 + "";
+            //}
+        }
+        catch
+        {
+            error.Text = "You are not logged in.";
+        }
+    }
+
+    //adding a comment for this image 
+    protected void commentButton_Click(object sender, EventArgs e)
+    {
+        u = Membership.GetUser();
+        //get comment box
+        TextBox commentBox = LoginView1.FindControl("AddCommentBox") as TextBox;
+        //create a comment literal
+        Literal myComment = new Literal();
+        myComment.Text = "<div class='comment' style='max-width:"+getWidth(id)+"px'><span class='commenter'><img src='/GetAvatar.ashx?user=" + u.UserName + "'/>&nbsp;<a href='/user/" + u.UserName + "'target='_blank'>" + u.UserName + "</a></span>" + commentBox.Text + "</div>";
+        //add to comment panel
+        commentPanel.Controls.AddAt(0, myComment);
+        // add it to the list
+        persistControls.Add(myComment);
+        // put it in the session
+        Session["persistControls"] = persistControls;
+        //clear comment box
+        commentBox.Text = "";
+
+        //add comment to database
+        /*
+        bool result = FuddleImage.comment(id, commentBox.Text);
+        if(result == false){
+            error.Text = "Error commenting."
+        }
+        //when comments are added to database remove from session.
+        else{
+             persistControls.Clear();    
+         }
+         */
+    }
+
+    protected void loadComments()
+    {
+        //FuddleImage.getComments(id);  //this should return all the comments for that image
+        //load comments below
     }
 }
