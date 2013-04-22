3510b702bd3ca0bc7410941364b3ddbe32759096
btevfik <bt@bu.edu>
2013-04-22 00:08:43 -0400

    remove console debug

--- scripts/uploadPreview.js
+++ scripts/uploadPreview.js
@@ -14,9 +14,7 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 });
 
 function previewImage(input) {
-
     $("#uploadStatus").css("display", "none");
-    console.log("AAA");
     var browserName = navigator.appName;
     if (browserName == "Microsoft Internet Explorer") {
         if (input.files && input.files[0]) {
