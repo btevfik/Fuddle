f7b4872a24d7095e44b8c26c739cca3d24ecfe82
btevfik <bt@bu.edu>
2013-04-27 02:29:33 -0400

    added ability to update title, delete images button to album page

--- Album.aspx
+++ Album.aspx
@@ -1,13 +1,39 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Album.aspx.cs" Inherits="Album" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <!-- StyleSheets -->
+    <link rel="stylesheet" type="text/css" href="/stylesheets/album.css" />
+    <!-- Scripts -->
+    <script src="/scripts/jquery.screwdefaultbuttonsV2.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.remove-box input:checkbox').screwDefaultButtons({
+                image: 'url("/resources/checkboxSmall.jpg")',
+                width: 43,
+                height: 43
+            });
+        });
+    </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
-    <div style="text-align: center">
-        <h1>
-            <asp:Label ID="AlbumTitle" runat="server" Text=""></asp:Label>
-        </h1>
-            by <asp:HyperLink ID="AlbumUser" runat="server"></asp:HyperLink>
+    <div id="container">
+        <div style="margin-left: 50px;">
+            <br />
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                <asp:Label ID="AlbumTitle" CssClass="album-title" runat="server" Text=""></asp:Label>
+                <asp:TextBox ID="NewAlbumTitle" runat="server" Visible="false"></asp:TextBox>
+                <asp:Button ID="UpdateTitleButton" CssClass="submitButton" OnClick="UpdateTitleButton_Click" Visible="false" runat="server" Text="Update Title" />
+                <asp:Button ID="SaveTitleButton" CssClass="submitButton" OnClick="SaveTitleButton_Click" Visible="false" runat="server" Text="Save" ValidationGroup="AlbumTitle" />
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="" ControlToValidate="NewAlbumTitle" ValidationGroup="AlbumTitle"><span style="color:#d14545;font-size:15px;">Title can't be empty.</span></asp:RequiredFieldValidator>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+            <div class="album-user">by
+                <asp:HyperLink ID="AlbumUser" runat="server"></asp:HyperLink></div>
+        </div>
+        <asp:Table ID="ImageTable" runat="server"></asp:Table>
+        <asp:Button ID="deleteSelectedButton" runat="server" CssClass="submitButton removeButton" Text="Remove Selected Images" OnClick="deleteSelectedButton_Click"  OnClientClick="if (!confirm('Are you sure you want to REMOVE selected images from the album? (The images will NOT be deleted)')) return false;" />
     </div>
 </asp:Content>
 
--- Album.aspx.cs
+++ Album.aspx.cs
@@ -4,27 +4,150 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 public partial class Album : System.Web.UI.Page
 {
+    //loggedin user
+    string loggedUser;
+    //album id
+    int albumId;
+    //user who ows this album
+    string user;
+
     protected void Page_Load(object sender, EventArgs e)
     {
         try
         {
-            int albumQuery = Int32.Parse(Request.QueryString["id"]);
-            string title = FuddleAlbum.getTitle(albumQuery);
-            string user = FuddleAlbum.getUser(albumQuery);
+            albumId = Int32.Parse(Request.QueryString["id"]);
+            string title = FuddleAlbum.getTitle(albumId);
+            user = FuddleAlbum.getUser(albumId);
             if (title == "" || user == "")
-                {
-                    throw new Exception();
-                }
+            {
+                throw new Exception();
+            }
             AlbumTitle.Text = title;
             AlbumUser.Text = user;
             AlbumUser.NavigateUrl = "/user/" + user;
+            //set the title of the page
+            Page.Header.Title = "Fuddle | " + title;
+
         }
         catch
         {
             Response.Redirect("/Oops.aspx?e=404");
         }
+
+        //get logged in user name
+        MembershipUser u = Membership.GetUser();
+        if (u != null)
+            loggedUser = u.UserName;
+
+        //load images of this album
+        loadImages();
+
+        //show update title button
+        if (loggedUser == user)
+        {
+            UpdateTitleButton.Visible = true;
+        }
+    }
+
+    protected void loadImages()
+    {
+        //List<int> imgs = FuddleAlbum.getImages(albumId);
+        //int rows = imgs.Count/5;
+        int rows = 10;
+        for (int k = 0; k < rows; k++)
+        {
+            TableRow row1 = new TableRow();
+            for (int i = 0; i < 4; i++)
+            {
+                try
+                {
+                    HyperLink imglink = new HyperLink();
+                    imglink.NavigateUrl = "/resources/gravatar.jpg";
+                    imglink.ImageUrl = "/resources/gravatar.jpg";
+                    imglink.CssClass = "album-img";
+                    TableCell c = new TableCell();
+                    c.Controls.Add(imglink);
+                    if (loggedUser == user)
+                    {
+                        CheckBox removeBox = new CheckBox();
+                        removeBox.ID = i + k*rows + "";
+                        removeBox.CssClass = "remove-box";
+                        c.Controls.Add(removeBox);
+                    }
+                    row1.Cells.Add(c);
+                }
+                catch
+                {
+                    break;
+                }
+                if (row1.Cells.Count > 0)
+                {
+                    ImageTable.Rows.Add(row1);
+                }
+            }
+        }
+    }
+
+    protected void UpdateTitleButton_Click(object sender, EventArgs e)
+    {
+        UpdateTitleButton.Visible = false;
+        SaveTitleButton.Visible = true;
+        NewAlbumTitle.Visible = true;
+    }
+    protected void SaveTitleButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            FuddleAlbum.updateTitle(NewAlbumTitle.Text, albumId);
+            AlbumTitle.Text = NewAlbumTitle.Text;
+            UpdateTitleButton.Visible = true;
+            SaveTitleButton.Visible = false;
+            NewAlbumTitle.Visible = false;
+        }
+        catch
+        {
+            //something bad ???
+        }
+    }
+    protected void deleteSelectedButton_Click(object sender, EventArgs e)
+    {
+        var boxes =  Page.GetAllControlsOfType<CheckBox>();
+        foreach (CheckBox checkbox in boxes)
+        {
+            //if checked remove from album
+            if (checkbox.Checked)
+            {
+                System.Diagnostics.Debug.WriteLine(checkbox.ID);
+            }
+            //reset boxes
+            checkbox.Checked = false;
+        }
+    }
+}
+
+/// <summary>
+///is used to get all the checkboxes in the page
+/// </summary>
+public static class ControlExtensions
+{
+    public static IEnumerable<T> GetAllControlsOfType<T>(this Control parent) where T : Control
+    {
+        var result = new List<T>();
+        foreach (Control control in parent.Controls)
+        {
+            if (control is T)
+            {
+                result.Add((T)control);
+            }
+            if (control.HasControls())
+            {
+                result.AddRange(control.GetAllControlsOfType<T>());
+            }
+        }
+        return result;
     }
 }
